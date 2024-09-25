33aea35edb3f91380fc25aceb52ed88c3c54077e
Oğuzhan Tomak <oguzhantomak@gmail.com>
2024-09-25 17:02:15 +0300

    Add new endpoint and handler for fetching vehicles
    Introduced a new endpoint in `GetVehiclesEndpoint.cs` under the
    namespace `Vehicle.API.GetVehicles`. This endpoint defines a route
    `/vehicles` for handling GET requests, utilizing an `ISender` to
    send a `GetVehiclesQuery` and returning a `GetVehiclesResponse`
    with a 200 OK status. Metadata for summary, description, and
    possible response types are included.
    
    In `GetVehiclesHandler.cs`, created the same namespace and defined
    `GetVehiclesQuery` and `GetVehiclesResult` records. Implemented
    `GetVehiclesQueryHandler` to handle the query, logging the process
    and retrieving the list of vehicles from `IDocumentSession`,
    returning them in a `GetVehiclesResult`.

---
+++ src/Services/Vehicle/Vehicle.API/GetVehicles/GetVehiclesEndpoint.cs
@@ -0,0 +1,23 @@
+﻿namespace Vehicle.API.GetVehicles;
+
+public record GetVehiclesResponse(IEnumerable<Models.Vehicle> Vehicles);
+public class GetVehiclesEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/vehicles",
+                async (ISender sender) =>
+                {
+                    var result = await sender.Send(new GetVehiclesQuery());
+
+                    var response = result.Adapt<GetVehiclesResponse>();
+
+                    return Results.Ok(response);
+                })
+            .WithName("GetVehicles")
+            .Produces<GetVehiclesResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("GetVehicles")
+            .WithDescription("Get all vehicles.");
+    }
+}
---
+++ src/Services/Vehicle/Vehicle.API/GetVehicles/GetVehiclesHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Vehicle.API.GetVehicles;
+
+public record GetVehiclesQuery() : IQuery<GetVehiclesResult>;
+public record GetVehiclesResult(IEnumerable<Models.Vehicle> Vehicles);
+
+public class GetVehiclesQueryHandler(IDocumentSession session, ILogger<GetVehiclesQueryHandler> logger) : IQueryHandler<GetVehiclesQuery, GetVehiclesResult>
+{
+    public async Task<GetVehiclesResult> Handle(GetVehiclesQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetVehiclesQueryHandler.Handle called with {@Query}", query);
+
+        var vehicles = await session.Query<Models.Vehicle>().ToListAsync(cancellationToken);
+
+        return new GetVehiclesResult(vehicles);
+    }
+}
